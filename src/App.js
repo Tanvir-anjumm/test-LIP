521ab3bc79fdf2ed1bb3aad0d04eb332bf04c0e5
Tanvir-anjumm <tanvir@katria.com>
2024-06-28 01:12:52 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './style.scss';
 import Container from './components/Container.js'
-import {internalIpV4} from 'internal-ip'
+// import {internalIpV4} from 'internal-ip'
 import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    console.log("local", getLocalIp())
-  }, [])
-  async function getLocalIp() {
-    console.log('Local Ip', await internalIpV4())
-  }
+  // useEffect(() => {
+  //   console.log("local", getLocalIp())
+  // }, [])
+  // async function getLocalIp() {
+  //   console.log('Local Ip', await internalIpV4())
+  // }
 
   return (
     <Container/>
